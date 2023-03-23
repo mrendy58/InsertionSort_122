6cc7b09b01ac9008ea616a5e9c64e86eed9b26df
mrendy58 <derendyyy58@gmail.com>
2023-03-23 16:10:57 +0700

    membuat void input

--- Insertion_Sort/Insertion_Sort.cpp
+++ Insertion_Sort/Insertion_Sort.cpp
@@ -4,6 +4,17 @@ using namespace std;
 int arr[20];        // membuat array dengan panjang data 20
 int n;              // membuat variable inputan n
 
+void input()
+{
+}
+
+
+
+
+
+
+
+
 int main()
 {
     std::cout << "Hello World!\n";
