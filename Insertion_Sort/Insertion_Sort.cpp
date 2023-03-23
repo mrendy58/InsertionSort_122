8b3e55e4a214f7511a0267a0e31e0437882c45f0
mrendy58 <derendyyy58@gmail.com>
2023-03-23 16:09:57 +0700

    membuat variable inputan n

--- Insertion_Sort/Insertion_Sort.cpp
+++ Insertion_Sort/Insertion_Sort.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std; 
 
+int arr[20];        // membuat array dengan panjang data 20
+int n;              // membuat variable inputan n
+
 int main()
 {
     std::cout << "Hello World!\n";
